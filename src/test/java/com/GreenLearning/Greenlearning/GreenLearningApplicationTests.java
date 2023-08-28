ee6c6c303e17b3f1836ab13e846ab01791cf3fbc
Pedro Henrique <pedrohenri1606@gmail.com>
2023-08-28 20:30:06 -0300

    Inicio de uma era

---
+++ src/test/java/com/GreenLearning/Greenlearning/GreenLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GreenLearning.Greenlearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
