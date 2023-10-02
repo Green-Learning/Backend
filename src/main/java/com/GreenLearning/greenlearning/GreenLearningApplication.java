ebc2b23ad1cc8c15386feee0d23f16323f8667fb
Pedro Henrique <pedrohenri1606@gmail.com>
2023-10-02 19:01:48 -0300

    Fix: Corrigido erros de inicialização

--- src/main/java/com/GreenLearning/greenlearning/GreenLearningApplication.java
+++ src/main/java/com/GreenLearning/greenlearning/GreenLearningApplication.java
@@ -1,4 +1,4 @@
-package com.GreenLearning.greenlearning;
+package com.greenLearning.greenlearning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
